202d064597e2f02da227340f051efac554401bac
NatiNegussie <136466532+NatiNegussie@users.noreply.github.com>
2023-09-05 20:19:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const start = () => {
+    const input = document.querySelector('.input');
+    const price1 = document.getElementById('price1').value;
+    const price2 = document.getElementById('price2').value;
+    const price3 = document.getElementById('price3').value;
+    const price4= document.getElementById('price4').value;
+    const price5 = document.getElementById('price5').value;
+    const price6 = document.getElementById('price6').value;
+    const budget = document.getElementById('budget').value;
+    input.style.display = 'flex';
+    document.getElementById('Start').style.display = 'none';
+   
+}
+const total = () => {
+    const input = document.querySelector('.input');
+    const price1 = document.getElementById('price1').value;
+    const price2 = document.getElementById('price2').value;
+    const price3 = document.getElementById('price3').value;
+    const price4= document.getElementById('price4').value;
+    const price5 = document.getElementById('price5').value;
+    const price6 = document.getElementById('price6').value;
+    const budget = document.getElementById('budget').value;
+    const p = document.getElementById('p');
+    
+    input.style.display = 'none';
+    p.style.display = 'flex';
+    if (budget <  budget - (price1 + price2 + price3 + price4 + price5 + price6)) {
+        alert("your budjet is less than your items price");
+    }
+  p.innerHTML = budget - (price1 + price2 + price3 + price4 + price5 + price6) + " Birr will be your total cost";
+}
